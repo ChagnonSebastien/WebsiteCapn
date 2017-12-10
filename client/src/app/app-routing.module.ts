c8c15cc1fd6eebfe64e8f7e80b1d1fa553b7d255
Unknown <chagnon.s21@gmail.com>
2017-12-10 12:30:54 -0500

    Router now retirects to Error component when a link does not exist

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: '**', redirectTo: '/' }
+    { path: 'not-found', component: ErrorComponent },
+    { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
