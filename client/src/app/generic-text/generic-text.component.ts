a96f58712532cbb2ab49d13bc6a05b848190d3c1
Unknown <chagnon.s21@gmail.com>
2017-12-11 09:39:56 -0500

    Added a quill editor to the generic text component

--- client/src/app/generic-text/generic-text.component.ts
+++ client/src/app/generic-text/generic-text.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quill } from 'quill';
 
 @Component({
   selector: 'app-generic-text',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenericTextComponent implements OnInit {
 
+  private editor: Quill;
+  public options =
+  {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],
+      [{ 'indent': '-1'}, { 'indent': '+1' }],
+      [{ 'align': [] }],
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+
+      ['link', 'image', 'video']
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      console.log();
+    }, 2000);
   }
 
+  onEditorCreated(event: Quill) {
+    this.editor = event;
+  }
+
+  save() {
+    console.log(this.editor.root.innerHTML);
+  }
 }
