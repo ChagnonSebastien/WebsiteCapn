fc3b542bf2c5a4c75243c3931b5d072bbe979361
Unknown <chagnon.s21@gmail.com>
2017-12-11 09:42:36 -0500

    Added a temporary routing path to the generic text component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { GenericTextComponent } from './generic-text/generic-text.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'temp', component: GenericTextComponent },
     { path: 'not-found', component: ErrorComponent },
     { path: '**', redirectTo: '/not-found' }
 ];
