fec81627af40b393aad2721fb72516679e0b4870
Unknown <chagnon.s21@gmail.com>
2017-12-11 09:36:49 -0500

    Added Quill the the package for text editing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { QuillModule } from 'ngx-quill';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +19,9 @@ import { GenericTextComponent } from './generic-text/generic-text.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    QuillModule
   ],
   providers: [],
   bootstrap: [AppComponent]
