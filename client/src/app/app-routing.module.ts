68c5015d01fc518a47b366cd95f75bdf0a9214f7
Unknown <chagnon.s21@gmail.com>
2017-12-20 14:52:45 -0500

    Implemented the routeResolverService and added it to the routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
+import { RouteResolverService } from './route-resolver.service';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const appRoutes: Routes = [
-    { path: 'app', loadChildren: 'app/user/user.module#UserModule'},
+    { path: 'app', loadChildren: 'app/user/user.module#UserModule', resolve: { routes: RouteResolverService } },
     { path: '', redirectTo: '/app', pathMatch: 'full' },
     { path: '**', redirectTo: '/app/not-found' }
 ];
--- client/src/app/route-resolver.service.ts
+++ client/src/app/route-resolver.service.ts
@@ -1,8 +1,27 @@
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router/src/router_state';
+import { Resolve } from '@angular/router/src/interfaces';
 import { Injectable } from '@angular/core';
+import { RouteNode } from './route-node';
 
 @Injectable()
-export class RouteResolverService {
+export class RouteResolverService implements Resolve<RouteNode> {
 
   constructor() { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<RouteNode> | Promise<RouteNode> | RouteNode {
+    return new Promise<RouteNode>((resolve) => {
+      const courseRoutes = new RouteNode('Cours', 'cours');
+      courseRoutes.children.push(new RouteNode('Cours pour enfants', 'enfants'));
+      courseRoutes.children.push(new RouteNode('Cours pour adultes', 'adultes'));
+
+      const routes = new RouteNode('Accueil', 'root');
+      routes.children.push(courseRoutes);
+      routes.children.push(new RouteNode('Camp de jour', 'camp'));
+      routes.children.push(new RouteNode('Formations de sauvetage', 'sauvetage'));
+
+      setTimeout(() => resolve(routes), 1000);
+    });
+  }
 
 }
