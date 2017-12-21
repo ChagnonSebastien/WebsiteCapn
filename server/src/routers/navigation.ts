dd6a541f5ab0717ab0e65a9b0615441e686b0724
Unknown <chagnon.s21@gmail.com>
2017-12-21 15:31:10 -0500

    /navigation/ now returns the navigation object in the database

--- server/src/routers/navigation.ts
+++ server/src/routers/navigation.ts
@@ -1,10 +1,16 @@
-import * as express from 'express';
+import { Database } from './../database';
+import { Router, Request, Response, NextFunction } from "express";
 
-const router = express.Router()
+const router = Router()
 
-// define the home page route
-router.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.send('NAVIGATION')
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  Database.getInstance()
+  .then(database => {
+    database.connection.collection('navigation').findOne('').then((result: any) => {
+      console.log(JSON.stringify(result));
+    });
+    res.send(database.connection.databaseName);
+  });
 })
 
 module.exports = router
