cc890121328cd2491d7fe3a40399d2fcca1e9d51
Unknown <chagnon.s21@gmail.com>
2017-12-21 14:28:40 -0500

    Added a config file for URLs and credentials

---
+++ server/src/config.ts
@@ -0,0 +1,6 @@
+// Database
+export const DATABASE_URL = 'localhost';
+export const DATABASE_PORT = 27017;
+export const DATABASE_DB = 'website';
+export const DATABASE_USER = 'root';
+export const DATABASE_PASSWORD = 'pwd123';
