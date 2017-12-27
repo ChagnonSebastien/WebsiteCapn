7fc645fa6dc938f855cfc7a8eb2b53165a48de29
Unknown <chagnon.s21@gmail.com>
2017-12-27 17:16:02 -0500

    Added https support forr the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as http from 'http';
+import * as https from 'https';
+import * as fs from 'fs';
 import * as cors from 'cors';
 import { Database } from './database';
 
@@ -7,21 +9,31 @@ const port = 5000;
 
 Database.getInstance().then(database => {
     if (database.connection) {
-        startServer();
+        startServer(process.argv.slice(2)[0] == 'https');
     } else {
         console.log('Error while starting the server. Exiting now.');
     }
 })
 
-function startServer(): http.Server {
+function startServer(isHttps: boolean): https.Server | http.Server {
     const app = express();
-
     config(app);
     routes(app);
 
-    return app.listen(port, () => {
-        console.log(`Server started on port ${port}`);
-    });
+    let server: https.Server | http.Server;
+    if (isHttps) {
+        const options = {
+            key: fs.readFileSync('/etc/letsencrypt/live/poutineqc.ca/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/poutineqc.ca/cert.pem')
+        };
+    
+        server = https.createServer(options, app).listen(port);
+    } else {
+        server = http.createServer(app).listen(port);
+    }
+
+    console.log(`Server started on port: ${port}`);
+    return server;
 }
 
 function config(app: express.Express): void {
