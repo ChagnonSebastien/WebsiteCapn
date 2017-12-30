865a682a9567a4e524c4714f84777e3a02ddfc35
Unknown <chagnon.s21@gmail.com>
2017-12-30 14:49:34 -0500

    Added a path in navigation on the server to get a page's content

--- server/src/routers/navigation.ts
+++ server/src/routers/navigation.ts
@@ -1,7 +1,7 @@
 import { Database } from './../database';
 import { Router, Request, Response, NextFunction } from "express";
 
-const router = Router()
+const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
   Database.getInstance()
@@ -10,6 +10,15 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
       res.send(result);
     });
   });
-})
+});
+
+router.get('/page/:path', (req: Request, res: Response, next: NextFunction) => {
+  Database.getInstance()
+  .then(database => {
+    database.connection.collection('pages').findOne({ _id: req.params.path }).then((result: any) => {
+      res.send(result.content);
+    });
+  });
+});
 
 module.exports = router;
