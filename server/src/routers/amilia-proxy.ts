71218fea8aae3ed76b800f96c4a55471bcb096bd
Unknown <chagnon.s21@gmail.com>
2017-12-30 12:56:49 -0500

    Added paths to get Programs and Activities from Amilia

--- server/src/routers/amilia-proxy.ts
+++ server/src/routers/amilia-proxy.ts
@@ -1,12 +1,24 @@
 import { Router, Request, Response, NextFunction } from "express";
 import * as request from 'request';
 
-const router = Router()
+const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     request('https://www.amilia.com/PublicApi/capn/fr/Programs', (error: any, response: request.RequestResponse, body: any) => {
         res.send(body);
     });
-})
+});
+
+router.get('/programs/:id', (req: Request, res: Response, next: NextFunction) => {
+    request(`https://www.amilia.com/PublicApi/capn/fr/Programs/${req.params.id}/Activities`, (error: any, response: request.RequestResponse, body: any) => {
+        res.send(body);
+    });
+});
+
+router.get('/activities/:id', (req: Request, res: Response, next: NextFunction) => {
+    request(`https://www.amilia.com/PublicApi/capn/fr/Activities/${req.params.id}`, (error: any, response: request.RequestResponse, body: any) => {
+        res.send(body);
+    });
+});
 
 module.exports = router;
