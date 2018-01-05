36ad3c9a3c09705e2c5b2a2681b36dcf5b05321a
Unknown <chagnon.s21@gmail.com>
2018-01-05 15:27:02 -0500

    Added body-parser as a middleware on the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
+import { Response, Request, NextFunction, Express } from 'express';
+import { readFileSync } from 'fs';
+import { json } from 'body-parser';
 import * as express from 'express';
 import * as http from 'http';
 import * as https from 'https';
-import * as fs from 'fs';
 import * as cors from 'cors';
+
 import { Database } from './database';
 
 const port = 5000;
@@ -23,8 +26,8 @@ function startServer(isHttps: boolean): https.Server | http.Server {
     let server: https.Server | http.Server;
     if (isHttps) {
         const options = {
-            key: fs.readFileSync('/etc/letsencrypt/live/poutineqc.ca/privkey.pem'),
-            cert: fs.readFileSync('/etc/letsencrypt/live/poutineqc.ca/cert.pem')
+            key: readFileSync('/etc/letsencrypt/live/poutineqc.ca/privkey.pem'),
+            cert: readFileSync('/etc/letsencrypt/live/poutineqc.ca/cert.pem')
         };
     
         server = https.createServer(options, app).listen(port);
@@ -36,11 +39,16 @@ function startServer(isHttps: boolean): https.Server | http.Server {
     return server;
 }
 
-function config(app: express.Express): void {
+function config(app: Express): void {
     app.use(cors());
+    app.use(json());
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        console.log(`${Date.now()}- ${req.method} request from ${req.connection.remoteAddress} at ${req.originalUrl}`);
+        next();
+    });
 }
 
-function routes(app: express.Express): void {
+function routes(app: Express): void {
     const navigation = require('./routers/navigation');
     app.use('/navigation', navigation);
     const amiliaProxy = require('./routers/amilia-proxy');
