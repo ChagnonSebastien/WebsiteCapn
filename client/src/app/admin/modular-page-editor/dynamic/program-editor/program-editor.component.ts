86ca1e4ad48db91a53677f42c10258d0ae14d9dc
Unknown <chagnon.s21@gmail.com>
2018-01-07 17:58:17 -0500

    The program-editor now extends the DynamicEditorComponent

--- client/src/app/admin/modular-page-editor/dynamic/program-editor/program-editor.component.ts
+++ client/src/app/admin/modular-page-editor/dynamic/program-editor/program-editor.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { DynamicComponent } from '../../../../user/modular-page/dynamic/dynamic.component';
+import { DynamicEditorComponent } from '../dynamic-editor.component';
 
 @Component({
   selector: 'app-program-editor',
   templateUrl: './program-editor.component.html',
   styleUrls: ['./program-editor.component.scss']
 })
-export class ProgramEditorComponent extends DynamicComponent implements OnInit {
+export class ProgramEditorComponent extends DynamicEditorComponent implements OnInit {
 
   constructor() {
     super();
