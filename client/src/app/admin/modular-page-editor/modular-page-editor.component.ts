cd05bd99cbd0187e39ba2ab381c5cd9f94683a08
Unknown <chagnon.s21@gmail.com>
2018-01-07 17:57:13 -0500

    The modular page editor can now add, remove and reorder the different modules on the page

--- client/src/app/admin/modular-page-editor/modular-page-editor.component.ts
+++ client/src/app/admin/modular-page-editor/modular-page-editor.component.ts
@@ -1,56 +1,98 @@
+import { ModalDirective } from 'ng-mdb-pro/free';
+import { Response } from 'express';
+import { AuthenticationService } from './../authentification.service';
+import { Http } from '@angular/http';
+import { GenericTextEditorComponent } from './../generic-text/generic-text-editor.component';
 import { ActivatedRoute, Data } from '@angular/router';
-import { Component, OnInit, ViewChild, ViewContainerRef, OnDestroy, ComponentRef, ComponentFactoryResolver, Type } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+  ComponentRef,
+  Type,
+  ElementRef
+} from '@angular/core';
 import { DynamicComponent } from '../../user/modular-page/dynamic/dynamic.component';
-import { GenericTextEditorComponent } from '../generic-text/generic-text-editor.component';
 import { ProgramEditorComponent } from './dynamic/program-editor/program-editor.component';
+import { SelectComponent } from 'ng-mdb-pro/pro/material-select';
+import { SortablejsDirective } from 'angular-sortablejs/dist';
+import { SERVER_URL, SERVER_PORT } from '../../config';
+import { UrlSegment } from '@angular/router/src/url_tree';
+import { DEFAULT_MODULE } from '../admin.config';
 
 @Component({
   selector: 'app-modular-page-editor',
-  template: '<div #container></div>'
+  templateUrl: './modular-page-editor.component.html',
+  styleUrls: ['./modular-page-editor.component.scss']
 })
-export class ModularPageEditorComponent implements OnInit, OnDestroy {
+export class ModularPageEditorComponent implements OnInit {
+
+  @ViewChild('success')
+  private successModal: ModalDirective;
+
+  @ViewChild('error')
+  private errorModal: ModalDirective;
+
+  @ViewChild('content')
+  private contentModal: ModalDirective;
 
   @ViewChild('container', { read: ViewContainerRef })
   container: ViewContainerRef;
 
-  private componentRefs: ComponentRef<{}>[];
+  @ViewChild('sortable')
+  sortable: SelectComponent;
 
-  constructor(private route: ActivatedRoute, private componentFactoryResolver: ComponentFactoryResolver) {
-    this.componentRefs = [];
+  private pageData: {}[];
+
+  constructor(private route: ActivatedRoute, private http: Http, private authenticationService: AuthenticationService) {
+    this.pageData = [];
   }
 
   public ngOnInit() {
     this.route.data.subscribe((data: Data) => {
-      this.componentRefs.forEach((component: ComponentRef<{}>) => {
-        component.destroy();
-      });
-      this.componentRefs = [];
-
-      data['pageData'].forEach((componentData: any) => {
-        const factory = this.componentFactoryResolver.resolveComponentFactory(this.getComponent(componentData.type));
-        const componentRef = this.container.createComponent(factory);
-        this.componentRefs.push(componentRef);
-
-        const instance = <DynamicComponent> componentRef.instance;
-        instance.context = componentData.context;
-      });
+      this.pageData = data['pageData'];
     });
   }
 
-  private getComponent(name: String): Type<{}> {
-    switch (name) {
-      case 'ProgramComponent':
-      return ProgramEditorComponent;
+  private savePageData() {
+    const path = this.route.snapshot.url.map((segment: UrlSegment) => {
+      return segment.path;
+    }).join('_');
 
-      case 'GenericTextComponent':
-      return GenericTextEditorComponent;
-    }
+    this.http
+      .post(`http://${SERVER_URL}:${SERVER_PORT}/navigation/page/${path}`, {
+        'token': this.authenticationService.getToken(),
+        'data': this.pageData
+      })
+      .toPromise()
+      .then((response: Response) => {
+        if (response.json().success) {
+          this.contentModal.hide();
+          this.successModal.show();
+        } else {
+          setTimeout(() => {
+            this.authenticationService.logOut();
+          }, 3000);
+          console.log(response.json().message);
+          this.errorModal.show();
+        }
+      })
+      .catch((reason: any) => {
+        console.log(reason);
+        this.errorModal.show();
+      });
   }
 
-  public ngOnDestroy() {
-    this.componentRefs.forEach((component: ComponentRef<{}>) => {
-      component.destroy();
+  private newModule(): void {
+    this.pageData.push({
+      'type': DEFAULT_MODULE,
+      'context': {}
     });
+  }
+
+  private deleteModule(index: number) {
+    this.pageData.splice(index, 1);
   }
 
 }
