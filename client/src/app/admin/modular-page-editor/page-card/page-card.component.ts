fbb95c9f6a3e3cec980ead813c480366e4934486
Unknown <chagnon.s21@gmail.com>
2018-01-26 17:25:33 -0500

    Added the visible attribute to a modular page that toggle it's visibility

--- client/src/app/admin/modular-page-editor/page-card/page-card.component.ts
+++ client/src/app/admin/modular-page-editor/page-card/page-card.component.ts
@@ -1,5 +1,5 @@
 import { SelectComponent } from 'ng-mdb-pro/pro/material-select';
-import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
 import { ADMIN_COMPONENTS } from '../../admin.config';
 
 @Component({
@@ -11,16 +11,25 @@ export class PageCardComponent implements OnInit {
 
   private optionsSelect: string[];
 
+  private randomToken: string;
+
   @Input('page') private page: any;
+
+  @ViewChild('checkbox') private checkbox: HTMLInputElement;
 
   @Output() private delete: EventEmitter<void>;
 
   constructor() {
+    this.randomToken = Math.random().toString();
     this.optionsSelect = Array.from(ADMIN_COMPONENTS.keys());
     this.delete = new EventEmitter<void>();
   }
 
   ngOnInit() {
+  }
+
+  private toogleVisibility() {
+    this.page.visible = !this.page.visible;
   }
 
   private deleteModule() {
